4ff21a6eeca17c9cfa567313f630f13ff6a4459a
sdst <1260144013@qq.com>
2015-05-29 14:59:10 +0800

    version 1.0

---
+++ web/src/main/java/com/st/test/index.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.st.test;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * @author st
+ *
+ */
+public class index {
+	private Logger log=Logger.getLogger(this.getClass());
+    
+    public String  login() {
+        log.info("hello  i am =====");
+        return "succerss";
+    }
+}
