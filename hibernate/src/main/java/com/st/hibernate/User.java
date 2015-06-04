02f60e3ef1bd38b08a79b0949f084583a1f07f94
sdst <1260144013@qq.com>
2015-06-04 16:34:33 +0800

    version 1.0

---
+++ hibernate/src/main/java/com/st/hibernate/User.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.st.hibernate;
+
+/**
+ * @author st
+ *
+ */
+public class User {
+	private int userid;
+	
+	private String userName;
+	
+	private String password;
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
